9d69380c462df45e48db9b554535454a973a7e29
Palaash0416 <palaash.jain@hotmail.com>
2025-08-05 18:30:37 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
+from fastapi import FastAPI, Request
 from fastapi.responses import HTMLResponse
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
-from fastapi import Request
+from pydantic import BaseModel
 
 app = FastAPI()
 app.mount("/static", StaticFiles(directory="static"), name="static")
